5bf110ad7e5358bcbee296cde881f09956058b16
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-27 17:23:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,13 +5,26 @@ class Song
   
   def initialize(title)
     @name = title
-    @@all << self #auto save
+    
   end
 
   def self.all #class method; function to accesss all instances of the call AKA CLASS READER METHOD
     @@all
   end
   
+  def self.destroy_all
+  @@all.clear #all.clear would work too
+  end
+  
+  def save
+    self.class.all << self #self is an instance; class method calls the class of the instance  #@@all << self #auto save
+  end
+
+  def self.create(title)
+    song = Song.new(title)
+    song.save #returns an array of sonsg, NOT an instance 
+    song #think RETURN VALUE; always want to return an instance of a song
+  end
 end
 
 #you can't ever access a class variable in an instance method cause it's out of scope; instance variable is scoped to an instance; 
